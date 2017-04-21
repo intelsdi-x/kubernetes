441e7ecdc948bc5f4137fe07e97931c82bbf7bea
Szymon scharmach <szymon.scharmach@intel.com>
2017-04-21 12:20:32 +0200

    Added extended isolation kubelet flag (#43)
    * Added extended isolation kubelet flag
    
    * Re-named flag to EnableExtendedIsolation
    
    * EventDispatcherNoop for disabled extended isolation
    
    * Simplified event dispatcher interface.

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -1491,9 +1491,7 @@ func (kl *Kubelet) syncPod(o syncPodOptions) error {
 	}
 
 	// TODO(squall0): Write down documentation here
-	if kl.eventDispacherEventChannel == nil {
-		kl.eventDispacherEventChannel = pcm.GetEventDispatcherChan()
-	}
+	kl.eventDispacherEventChannel = pcm.GetEventDispatcherChan()
 
 	// Create Mirror Pod for Static Pod if it doesn't already exist
 	if kubepod.IsStaticPod(pod) {
