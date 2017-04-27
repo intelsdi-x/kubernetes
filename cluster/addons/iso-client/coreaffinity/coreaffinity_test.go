c664a751d66db4258a1f6204508c67516e3647e5
Michał Stachowski <michal.stachowski@intel.com>
2017-04-27 11:22:19 +0200

    Coreaff isolator

--- cluster/addons/iso-client/coreaffinity/coreaffinity_test.go
+++ cluster/addons/iso-client/coreaffinity/coreaffinity_test.go
@@ -1,1 +1,38 @@
 package coreaffinity
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCoreAffinityIsolator_Name(t *testing.T) {
+	testCases := []string{
+		"foo",
+		"bar",
+		"⛱⛱⛱⛱",
+		"\x00\xA0\x00\xA0\x00\xA0",
+		"\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98",
+	}
+	for _, testCase := range testCases {
+		cfa, err := New(testCase)
+		assert.Nil(t, err, fmt.Sprintf("Cannot create coreAffinityIsolator: %q", err.Error()))
+		name := cfa.Name()
+		assert.Equal(t, testCase, name, fmt.Sprintf("Isolator name %q is not as expected name %q", name, testCase))
+	}
+}
+
+type mockedOpaque struct {
+	mock.Mock
+}
+
+func (m *mockedOpaque) AdvertiseOpaqueResource(name string, value int) error {
+	args := m.Called(name, value)
+	return args.Error(0)
+}
+
+func TestCoreAffinityIsolator_ShutDown(t *testing.T) {
+	testObj :=new(mockedOpaque)
+}
