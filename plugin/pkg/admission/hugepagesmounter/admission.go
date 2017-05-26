46029b30a66b68a84bf770fde7b5a79ea9195b9f
PiotrProkop <piotr.prokop@intel.com>
2017-05-26 15:41:12 +0200

    Add usage of resource.Quantity

--- plugin/pkg/admission/hugepagesmounter/admission.go
+++ plugin/pkg/admission/hugepagesmounter/admission.go
@@ -19,10 +19,6 @@ package hugepagesmounter
 import (
 	"fmt"
 	"io"
-	"strconv"
-	"strings"
-
-	"github.com/golang/glog"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -32,9 +28,8 @@ import (
 )
 
 const (
-	annotationPrefix = "hugepagesmounter.admission.kubernetes.io"
-	pluginName       = "HugePagesMounter"
-	volumeNamePrefix = "hugepagesmounter-"
+	annotationPrefix = "hugepagesvolumeresources.admission.kubernetes.io"
+	pluginName       = "hugepagesVolumeResources"
 )
 
 func init() {
@@ -46,7 +41,6 @@ func init() {
 // hugePagesMounterPlugin is an implementation of admission.Interface.
 type hugePagesMounterPlugin struct {
 	*admission.Handler
-	//client internalclientset.Interface
 }
 
 // NewPlugin creates a new hugePagesMounter admission plugin.
@@ -75,7 +69,7 @@ func (c *hugePagesMounterPlugin) Admit(a admission.Attributes) error {
 			if err != nil {
 				return fmt.Errorf("Cannot calculate hugePages size for %v : %v", volume.Name, err)
 			}
-			modifyContainers(hugepagesCount, pod.Spec.Containers, volume.Name)
+			fillResourcesFor(pod.Spec.Containers, volume.Name, hugepagesCount)
 
 		}
 	}
@@ -83,10 +77,11 @@ func (c *hugePagesMounterPlugin) Admit(a admission.Attributes) error {
 	return nil
 }
 
-func modifyContainers(hugepagesCount int64, containers []api.Container, volumeName string) {
+func fillResourcesFor(containers []api.Container, volumeName string, hugepagesCount int64) {
 	for containerID := range containers {
 		if containerHasHugepagesVolume(volumeName, containers[containerID]) {
 			requests := containers[containerID].Resources.Requests
+			// if no Resource.Requests are specified it's a nil map
 			if requests == nil {
 				requests = make(api.ResourceList)
 			}
@@ -99,8 +94,6 @@ func modifyContainers(hugepagesCount int64, containers []api.Container, volumeNa
 
 			requests[hugePagesResourceName] = *resource.NewQuantity(newValue, resource.DecimalSI)
 			// make sure we store new request in case it was empty
-
-			glog.Infof("Request is : %#v\n", requests)
 			containers[containerID].Resources.Requests = requests
 		}
 	}
@@ -116,30 +109,19 @@ func containerHasHugepagesVolume(volumeName string, container api.Container) boo
 }
 
 func calculatePagesCount(maxSize string) (int64, error) {
-	// TODO(pprokop): add support for other sizes
-	glog.Infof("-----------Last char: %s\n", string(maxSize[len(maxSize)-1]))
-	if !strings.Contains("mM", string(maxSize[len(maxSize)-1])) {
-		return 0, fmt.Errorf("Not supported size of HugePages not: %v", maxSize[len(maxSize)-1])
-	}
-	glog.Infof("-------------------------------maxSize: %s", maxSize)
-
-	size, err := strconv.Atoi(maxSize[:len(maxSize)-1])
-
+	quantity, err := resource.ParseQuantity(maxSize)
 	if err != nil {
-		return int64(size), fmt.Errorf("Only available sizes are m and M not: %v", maxSize)
+		return 0, err
 	}
 
-	glog.Infof("SIze is : %v\n", size)
-
-	size1 := convertMaxSizeTo2MHugePages(size)
-	glog.Infof("SIze1 is : %v\n", size1)
-	return size1, nil
+	return convertMaxSizeTo2MHugePages(quantity.ScaledValue(resource.Mega)), nil
 
 }
 
-func convertMaxSizeTo2MHugePages(maxSize int) int64 {
+func convertMaxSizeTo2MHugePages(maxSize int64) int64 {
 	if maxSize%2 != 0 {
-		return int64((maxSize + 1) / 2)
+		// round to multiplication of 2
+		return (maxSize + 1) / 2
 	}
-	return int64(maxSize / 2)
+	return maxSize / 2
 }
